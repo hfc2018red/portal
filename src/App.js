158529fff9b909165976ce5180d8bdfbd13d4d86
Mike Lay <mike@mkly.io>
2018-10-20 04:11:31 -0700

    Messages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      message: '',
+      messages: [],
+      threadId: undefined
+    }
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    window.fetch('https://hfc2018red.herokuapp.com/message', {
+      method: 'POST',
+      body: JSON.stringify({
+        body: this.state.message,
+        threadId: this.state.threadId
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).then(res => {
+      return res.json();
+    }).then(json => {
+      if (!json) {
+        console.log('bad json');
+        return;
+      }
+
+      this.setState(Object.assign({}, this.state, {
+        messages: json.messages || [],
+        message: '',
+        threadId: json.uuid || undefined
+      }));
+    });
+  }
+
+  handleChange(e) {
+    this.setState(Object.assign({}, this.state, {
+      message: e.target.value
+    }));
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        {this.state.messages.length === 0 &&
+          <div>
+            <label>I am looking for...</label>
+            <input type="text" value={this.state.message} onChange={this.handleChange.bind(this)} />
+            <button onClick={this.handleSubmit.bind(this)} disabled={this.state.message.length === 0}>Send</button>
+          </div>
+        }
+        <ul>
+        {this.state.messages.map(m => {
+          return (
+            <li>{m.body}</li>
+          )
+        })}
+        </ul>
+        {this.state.messages.length > 0 &&
+          <div>
+            <input type="text" value={this.state.message} onChange={this.handleChange.bind(this)} />
+            <button onClick={this.handleSubmit.bind(this)} disabled={this.state.message.length === 0}>Send</button>
+          </div>
+        }
       </div>
     );
   }
